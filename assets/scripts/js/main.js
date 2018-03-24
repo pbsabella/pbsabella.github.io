03c73e9747975832e292ebb16fe7499dbc21cb79
pbsabella <pbsabella@github.com>
2018-03-24 11:28:39 +0100

    Initialize ScrollSpy
    - Clicking the nav items should ease to the relevant section

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,3 +1,14 @@
-// Initialize Material side nav
-var sidenav = document.querySelector('.sidenav');
-var instance = M.Sidenav.init(sidenav);
+// Initialize SideNav
+var sidenavOptions = {
+  preventScrolling: true
+};
+var sidenavElem = document.querySelector('.sidenav');
+var sidenavInstance = M.Sidenav.init(sidenavElem, sidenavOptions);
+
+// Initialize ScrollSpy
+var scrollspyOptions = {
+  activeClass: 'is-active',
+  scrollOffset: 10
+};
+var scrollspyElem = document.querySelectorAll('.scrollspy');
+var scrollspyInstance = M.ScrollSpy.init(scrollspyElem, scrollspyOptions);
