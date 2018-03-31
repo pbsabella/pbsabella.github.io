75ee685aa4aeb74266935aa4f3a2be1581c21a21
pbsabella <pbsabella@github.com>
2018-04-01 02:32:12 +0800

    Delay animation on page titles

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -22,7 +22,18 @@ function initializeMaterialComponents() {
 
 function isElementSeen (el) {
   var rect = el.getBoundingClientRect();
-  var offset = rect.height / 2;
+  var offset = 0;
+
+  if (rect.height > 100) {
+    // Element is seen when half of the content is visible.
+    // Used for larger elements so it will speed up visibility
+    // rather than waiting for the whole content to be visible
+    offset = rect.height / 2;
+  } else {
+    // Element is seen when it is over the content height
+    // Used for smaller elements so it will delay visibility
+    offset = (rect.height * 2) * -1;
+  }
 
   return (
       (rect.bottom - offset) <= (window.innerHeight || document.documentElement.clientHeight)
