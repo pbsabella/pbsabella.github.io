700c479109700ca7a51964d0dbd61a39c94f739b
Paula Bianca Abella <pau.abella@gmail.com>
2018-10-07 17:35:55 +0800

    Switch to Webpack
    - Remove Gulp and relevant packages
    - Add webpack and relevant packages
    - Remove Font Awesome

---
+++ src/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+import './styles/main.scss';
+
+// Elements
+var headerElem = document.querySelector('.Header');
+var sidenavElem = document.querySelector('.sidenav');
+var scrollspyElem = document.querySelectorAll('.scrollspy');
+var animateElem = document.querySelectorAll('.animate');
+
+function initializeMaterialComponents() {
+  // Initialize SideNav
+  var sidenavOptions = {
+    edge: 'right'
+  };
+
+  // Initialize ScrollSpy
+  var scrollspyOptions = {
+    activeClass: 'is-active',
+    scrollOffset: 80
+  };
+
+  M.Sidenav.init(sidenavElem, sidenavOptions);
+  M.ScrollSpy.init(scrollspyElem, scrollspyOptions);
+}
+
+function isElementSeen (el) {
+  var rect = el.getBoundingClientRect();
+  var offset = 0;
+
+  if (rect.height > 100) {
+    // Element is seen when half of the content is visible.
+    // Used for larger elements so it will speed up visibility
+    // rather than waiting for the whole content to be visible
+    offset = rect.height / 2;
+  } else {
+    // Element is seen when it is over the content height
+    // Used for smaller elements so it will delay visibility
+    offset = (rect.height * 2) * -1;
+  }
+
+  return (
+      (rect.bottom - offset) <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
+function animateElements() {
+  animateElem.forEach(function (elem) {
+    if (isElementSeen(elem)) {
+      elem.classList.add('is-visible');
+    } else {
+      elem.classList.remove('is-visible');
+    }
+  });
+}
+
+function toggleTransparentHeader() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop === 0) {
+    headerElem.classList.add('is-transparent');
+  } else {
+    headerElem.classList.remove('is-transparent');
+  }
+}
+
+document.addEventListener('scroll', function () {
+  animateElements();
+  toggleTransparentHeader();
+});
+
+animateElements();
+initializeMaterialComponents();
+toggleTransparentHeader();
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          }
+        }]
+      },
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './main.css'
+    })
+  ],
+
+  devServer: {
+    contentBase: './dist'
+  },
+};
