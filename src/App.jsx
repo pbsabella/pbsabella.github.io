ff93d388b777a95f4f4cda8c54a01d2a64e75ba3
Paula Bianca Abella <pau.abella@gmail.com>
2026-01-28 21:30:42 +0800

    Add catch-all redirect to unknown paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Styleguide from './pages/Styleguide';
@@ -11,8 +11,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/labs/styleguide" element={<Styleguide />} />
-                    {/* Labs main page can be added later */}
-                    <Route path="/labs" element={<Home />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
             </Layout>
         </Router>
